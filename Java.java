3b6c6c1e6e705cf8cec355b2209d4e1f37697c3d
Zulfikar <zulfikar97@gmail.com>
2015-11-24 19:17:10 +0700

    Update Java.java

--- Java.java
+++ Java.java
@@ -3,12 +3,22 @@ import java.io.*;
 public class Java {
     public static void main (String []args) throws Exception{
         BufferedReader masuk = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Masukkan nilai argumen 1 (1 atau 0) : ");
-        int a1 = Integer.parseInt(masuk.readLine());
-        System.out.print("Masukkan nilai argumen 2 (1 atau 0) : ");
-        int a2 = Integer.parseInt(masuk.readLine());
-        int pil=0;
-        while (pil != 5) {
+        int a1, a2;
+        a1=3;a2=3;
+        while ((a1!=0)&&(a1!=1)){
+        System.out.print("Masukkan nilai p (1 atau 0) : ");
+        a1 = Integer.parseInt(masuk.readLine());
+        if ((a1!=0)&&(a1!=1))
+            System.out.println("Pastikan input yang anda masukkan benar (1 atau 0)\n");
+        }
+        while ((a2!=0)&&(a2!=1)){
+        System.out.print("Masukkan nilai q (1 atau 0) : ");
+        a2 = Integer.parseInt(masuk.readLine());
+        if ((a2!=0)&&(a2!=1))
+            System.out.println("Pastikan input yang anda masukkan benar (1 atau 0)\n");
+        }
+        String stpil="0";
+        while (!stpil.equals("5")) {
             System.out.println("=================================");
             System.out.println("         Daftar Proposisi        ");
             System.out.println("=================================");
@@ -18,35 +28,36 @@ public class Java {
             System.out.println(" 4. Biimplikasi");
             System.out.println(" 5. Keluar \n");
             System.out.print("Masukkan pilihan : ");
-            pil = Integer.parseInt(masuk.readLine());
-            switch (pil) {
-                case 1:
+            stpil = masuk.readLine();
+            switch (stpil) {
+                case "1":
                     if ((a1==1) && (a2==1))
                         System.out.println( "Proposisi " + a1+" ^ "+a2 + " bernilai benar. \n");
                     else
                         System.out.println( "Proposisi " + a1+" ^ "+a2 + " bernilai salah. \n");
                     break;
-                case 2:
+                case "2":
                     if ((a1==0) && (a2==0))
                         System.out.println( "Proposisi " + a1+" v "+a2 + " bernilai salah. \n");
                     else
                         System.out.println( "Proposisi " + a1+" v "+a2 + " bernilai benar. \n");
                     break;
-                case 3:
+                case "3":
                     if ((a1==1) && (a2==0))
                         System.out.println( "Proposisi " + a1+" --> "+a2 + " bernilai salah. \n");
                     else
                         System.out.println( "Proposisi " + a1+" --> "+a2 + " bernilai benar. \n");
                     break;
-                case 4:
+                case "4":
                     if (a1==a2)
                         System.out.println( "Proposisi " + a1+" <--> "+a2 + " bernilai benar. ");
                     else
                         System.out.println( "Proposisi " + a1+" <--> "+a2 + " bernilai salah. ");
                     break;
-                case 5:
+                case "5":
                     break;
                 default:
+                    System.out.println("Pastikan input yang anda masukkan benar (1-5)\n\n");
                     break;
             }
         }
